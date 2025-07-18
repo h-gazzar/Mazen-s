083d9a72aeaf43410f97b6aebc2c50493f5a65fb
h-gazzar <h.elgazzar81@gmail.com>
2025-07-18 14:20:59 +0300

    dew

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Header from './Header.jsx'
+import video from './assets/video'
 
 function App() {
-  
+  return(
+    <Header></Header>
+  );
 }
 
 export default App
