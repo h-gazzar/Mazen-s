4ef4d1541186faca4d1b68da339ef8975a56b08f
h-gazzar <h.elgazzar81@gmail.com>
2025-07-18 15:48:55 +0300

    ;

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './Header.jsx'
-import video from './assets/video'
+import video from '../public/vecteezy_ocean-day-to-night-timelapse_23152644.mp4'
+import VideoPlayer from './VideoPlayer.jsx'
 
 function App() {
   return(
-    <Header></Header>
+    <div className='main'>
+          <Header></Header>
+          <VideoPlayer />
+
+    </div>
   );
 }
 
